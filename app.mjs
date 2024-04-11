fd4fbb863ffe25ed094d40cc54b03d84b72a21b0
Ishan Pranav <ishan.pranav@outlook.com>
2024-04-11 17:42:54 -0400

    Follow config instructions

--- app.mjs
+++ app.mjs
@@ -2,7 +2,8 @@
 // Copyright (c) 2024 Ishan Pranav
 // Licensed under the MIT license.
 
-import { config } from 'dotenv';
+import './config.mjs'; // first
+
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
@@ -11,6 +12,5 @@ const app = express();
 const fileName = fileURLToPath(import.meta.url);
 const directoryName = join(dirname(fileName), 'dist');
 
-config();
 app.use(express.static(directoryName));
-app.listen(process.env.PORT || 30001);
+app.listen(process.env.PORT || 3000);
